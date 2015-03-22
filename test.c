2d4864575222b240c75f3d01c9629804e7efba9f
vikram <nadathurvikram@gmail.com>
2015-03-22 13:47:20 -0700

    send
    sent an email to dad

--- test.c
+++ test.c
@@ -2,11 +2,19 @@
 #include <conio.h>
 #include <stdlib.h>
 int main(){
-
+	int a,b,c;
+	char d,e,f;
+	long alpahbet="abcdefghijklmnopqrstuvwxyz";
+	d="test 1";
+	e="test 2";
+	f="test 3";
+	
+	printf(alpahbet);
 	printf("Your score is %d \n", 100);
     printf("My score is %f \n", 150.50);
-
-
+	prinf(d);
+	strcpy(d, "test 4");
+	printf(d);
 
 
 	return 0;
