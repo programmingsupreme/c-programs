7396a7a5abb4fdcdab82017496b187e8e311b517
vikram <nadathurvikram@gmail.com>
2015-03-23 16:55:23 -0700

    test.exe
    windows error

--- test.c
+++ test.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 int main(){
 	int a,b,c;
-	char d,e,f;
-	char* alpahbet="abcdefghijklmnopqrstuvwxyz";
+	char* d,e,f;
+	char alpahbet[]="abcdefghijklmnopqrstuvwxyz";
 	d="test 1";
 	e="test 2";
 	f="test 3";
@@ -12,7 +12,7 @@ int main(){
 	printf(alpahbet);
 	printf("Your score is %d \n", 100);
     printf("My score is %f \n", 150.50);
-	prinf(d);
+	printf(d);
 	strcpy(d, "test 4");
 	printf(d);
 
