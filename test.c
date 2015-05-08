374eb540e8927db7f825bbc732337f3467e28626
vikram <nadathurvikram@gmail.com>
2015-05-07 18:47:53 -0700

    test
    age error

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <stdlib.h>
 #include  "test.h"
+#include <string.h>
 int main(){
 	int a,b,c;
 	char e[20];
@@ -14,13 +15,18 @@ int main(){
     printf("My score is %f \n", 150.50);
 	printf(d);
 	strcpy(d, "test 4\n");
+	strcpy(d, "test 5");
 	printf(d);
 	printf("My name is %s", MYNAME);
 	printf("What is your name?");
 	scanf("%s",&e);
-	printf("Your name is %s right?", e);;
+	printf("Your name is %s right?", e);
 	scanf("%s", f);
-	
+	printf("I am %s years old",AGE);
+	printf("How old are you?");
+	scanf("%s", b);
+
+
 	
 	
 	
